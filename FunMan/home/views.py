e6712e79f5c5ce0fc3f1d45430c68d8e52b445e7
Mitsuhano <mitsuhanocreations@gmail.com>
2025-02-27 21:18:35 +0200

    feat: creation posts update

--- FunMan/home/views.py
+++ FunMan/home/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render, redirect
-from .forms import *
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required 
+from .forms import PostForm
+from .models import Post 
+
+
+def profile(request):
+    posts = Post.objects.filter(user=request.user).order_by("-date")
+
+    return render(request, "home/profile.html", {"posts": posts})
 
 
 @login_required
 def home(request):
-    return render(request, "home/home.html")
-
-
-@login_required
-def profile(request):
-    user = request.user
+    posts = Post.objects.filter(user=request.user).order_by("-date")
     
-    context = {
-        "user" : user
-    }
-    return render(request, "home/profile.html", context)
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect("home")  
+    else:
+        form = PostForm()
+    return render(request, "home/home.html", {"form": form, "posts": posts})
+
+
+# @login_required
+# def profile(request):
+#     user = request.user
+    
+#     context = {
+#         "user" : user
+#     }
+#     return render(request, "home/profile.html", context)
 
 
 @login_required
